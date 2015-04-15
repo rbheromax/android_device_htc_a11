66351f8c0846e42587ddf4baca9470faeff3e720
Czar Dixon <rbheromax@gmail.com>
2015-04-14 21:00:16 -0400

    android-5.1 device tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,5 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/full_a11.mk \
+    $(LOCAL_DIR)/cm.mk \
+    $(LOCAL_DIR)/device.mk
+
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+add_lunch_combo full_a11-eng
+add_lunch_combo full_a11-userdebug
