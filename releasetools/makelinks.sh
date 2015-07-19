3cb3d5312ee32328429a0ad9d3ccab93fe47792b
Greg Willard <wickett06@gmail.com>
2015-07-19 10:31:10 -0400

    a11: Meh

--- releasetools/makelinks.sh
+++ releasetools/makelinks.sh
@@ -16,10 +16,7 @@ if [ $variant == "gsm" ]; then
 fi
 
 if [ $variant == "chl" ]; then
-	# dualsim variant uses prebuilt ril blobs
-	rm -f /system/bin/rild
-	rm -f /system/lib/libril.so
-	NFCFILES="app/NfcNci lib/libnfc-nci.so lib/libnfc_nci_jni.so lib/libnfc_ndef.so lib/hw/nfc_nci.pn54x.default.so etc/libnfc-nxp.conf permissions/com.cyanogenmod.nfc.enhanced.xml etc/permissions/com.android.nfc_extras.xml etc/permissions/android.hardware.nfc.xml etc/libnfc-brcm.conf etc/nfcee_access.xml framework/com.android.nfc_extras.jar priv-app/Tag vendor/firmware/libpn547_fw.so"
+	# I think we should remove nothing for now and call it good. :)
 
 	for i in $NFCFILES; do
 		rm -rf /system/$i
