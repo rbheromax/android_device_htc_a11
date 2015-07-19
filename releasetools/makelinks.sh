1ba5c1f6691586a3397796d60c081257a52952b8
Greg Willard <wickett06@gmail.com>
2015-07-19 11:37:19 -0400

    a11: Ha! Gotcha!

--- releasetools/makelinks.sh
+++ releasetools/makelinks.sh
@@ -1,27 +1,27 @@
 #!/sbin/sh
 
-set -e
+# set -e
 
-modelid=`getprop ro.boot.mid`
+# modelid=`getprop ro.boot.mid`
 
-case $modelid in
-	OPCV1000|OPCV2000)		variant="chl" ;;
-	*)				variant="gsm" ;;
-esac
+# case $modelid in
+# 	OPCV1000|OPCV2000)		variant="chl" ;;
+#	*)				variant="gsm" ;;
+# esac
 
-if [ $variant == "gsm" ]; then
+# if [ $variant == "gsm" ]; then
 	# remove prebuilt ril blobs
-	rm -rf /system/blobs/gsm/lib
-	rm -f /system/blobs/gsm/bin/rild
-fi
+# 	rm -rf /system/blobs/gsm/lib
+#	rm -f /system/blobs/gsm/bin/rild
+#  fi
 
-if [ $variant == "chl" ]; then
+# if [ $variant == "chl" ]; then
 	# I think we should remove nothing for now and call it good. :)
 
-	variant="gsm"
-fi
+# 	variant="gsm"
+# fi
 
-basedir="/system/blobs/$variant/"
-cd $basedir
-chmod 755 bin/*
-find . -type f | while read file; do ln -s $basedir$file /system/$file ; done
+# basedir="/system/blobs/$variant/"
+# cd $basedir
+# chmod 755 bin/*
+# find . -type f | while read file; do ln -s $basedir$file /system/$file ; done
