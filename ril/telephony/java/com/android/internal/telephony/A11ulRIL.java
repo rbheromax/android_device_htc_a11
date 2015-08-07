8786ee851d81969e97bfde810e997c51ee0a298f
Greg Willard <Wickett06@gmail.com>
2015-08-07 15:24:21 -0400

    Separate the RIL classes (1/3)

---
+++ ril/telephony/java/com/android/internal/telephony/A11ulRIL.java
@@ -0,0 +1,18 @@
+package com.android.internal.telephony;
+
+import android.content.Context;
+
+public class A11ulRIL extends RIL implements CommandsInterface {
+
+    public A11ulRIL(Context context, int networkMode, int cdmaSubscription) {
+        super(context, networkMode, cdmaSubscription, null);
+        mQANElements = 5;
+    }
+
+    public A11ulRIL(Context context, int preferredNetworkType,
+            int cdmaSubscription, Integer instanceId) {
+        super(context, preferredNetworkType, cdmaSubscription, instanceId);
+        mQANElements = 5;
+    }
+
+}
