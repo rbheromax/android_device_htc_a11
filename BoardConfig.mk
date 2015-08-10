3b769e24ac16ae82f5eb1b0a2fdece189785d0b0
r3pwn (Greg Willard) <wickett06@gmail.com>
2015-08-10 01:13:00 -0400

    a11: use msm8974 kernel tree instead

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,8 +48,8 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02008000 --tags_offset 0x01e00000
 BOARD_CUSTOM_BOOTIMG_MK := device/htc/a11/mkbootimg.mk
-TARGET_KERNEL_CONFIG := cyanogenmod_a11_defconfig
-TARGET_KERNEL_SOURCE := kernel/htc/a11
+TARGET_KERNEL_CONFIG := cm_a11ul_defconfig
+TARGET_KERNEL_SOURCE := kernel/htc/msm8974
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
