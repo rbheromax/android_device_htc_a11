9e1dadf9cb4d8f7a581e4b4974fbc0da15194915
Greg Willard <Wickett06@gmail.com>
2015-08-12 21:26:12 -0400

    makelinks: fixups
    - Now detects models correctly
    - No longer removes blobs
    - No longer tells chl it's gsm

--- releasetools/makelinks.sh
+++ releasetools/makelinks.sh
@@ -5,26 +5,16 @@ set -e
 modelid=`getprop ro.boot.mid`
 
 case $modelid in
-	OPCV1000|OPCV2000)		variant="chl" ;;
+	OPCV10000|OPCV20000)		variant="chl" ;;
 	*)				variant="gsm" ;;
 esac
 
-if [ $variant == "gsm" ]; then
-	# remove prebuilt ril blobs
-	rm -rf /system/blobs/gsm/lib
-	rm -f /system/blobs/gsm/bin/rild
-fi
-
 if [ $variant == "chl" ]; then
-	# dualsim variant uses prebuilt ril blobs
-	rm -f /system/bin/rild
-	rm -f /system/lib/libril.so
 	NFCFILES="app/NfcNci lib/libnfc-nci.so lib/libnfc_nci_jni.so lib/libnfc_ndef.so lib/hw/nfc_nci.pn54x.default.so etc/libnfc-nxp.conf permissions/com.cyanogenmod.nfc.enhanced.xml etc/permissions/com.android.nfc_extras.xml etc/permissions/android.hardware.nfc.xml etc/libnfc-brcm.conf etc/nfcee_access.xml framework/com.android.nfc_extras.jar priv-app/Tag vendor/firmware/libpn547_fw.so"
 
 	for i in $NFCFILES; do
 		rm -rf /system/$i
 	done
-	variant="gsm"
 fi
 
 basedir="/system/blobs/$variant/"
