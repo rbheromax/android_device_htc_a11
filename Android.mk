8503e6e81514bb837d1d570e5aac52ed3f18aa13
r3pwn (Greg Willard) <wickett06@gmail.com>
2015-09-07 02:27:22 -0400

    a11: Fix/un-break un-break = I broke something and am fixing it now. fix = try to fix something that was broken from the start.

--- Android.mk
+++ Android.mk
@@ -24,9 +24,10 @@ PRODUCT_MAKEFILES := AndroidProducts.mk
 # Modem image symlink
 MODEM_IMAGES := \
     modem.b00 modem.b01 modem.b02 modem.b03 modem.b04 modem.b05 \
-    modem.b08 modem.b10 modem.b11 modem.b13 modem.b14 modem.b15 \
-    modem.b16 modem.b17 modem.b18 modem.b19 modem.b20 modem.b21 \
-    modem.b22 modem.b25 modem.mdt
+    modem.b06 modem.b07 modem.b08 modem.b09 modem.b10 modem.b11 \
+    modem.b12 modem.b13 modem.b14 modem.b15 modem.b16 modem.b17 \
+    modem.b18 modem.b19 modem.b20 modem.b21 modem.b22 modem.b23 \
+    modem.b24 modem.b25 modem.b26 modem.b27 modem.b28 modem.mdt
 
 MODEM_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(MODEM_IMAGES)))
 $(MODEM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
@@ -38,30 +39,17 @@ $(MODEM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 ALL_DEFAULT_INSTALLED_MODULES += $(MODEM_SYMLINKS)
 
 # MBA image symlink
-MBA_IMAGES := \
-    mba.b00 mba.mdt
-
-MBA_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(MBA_IMAGES)))
-$(MBA_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "MBA firmware link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /firmware/radio/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(MBA_SYMLINKS)
-
-# A9180E1 image symlink (not sure what this is, but it might be needed; it's with the radio stuff...)
-A9180E1_IMAGES := \
-    a9180e1.b00 A9180E1.mdt
-
-A9180E1_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(A9180E1_IMAGES)))
-$(A9180E1_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "A9180E1 firmware link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /firmware/radio/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(A9180E1_SYMLINKS)
+#MBA_IMAGES := \
+#    mba.b00 mba.mdt
+#
+#MBA_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(MBA_IMAGES)))
+#$(MBA_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+#	@echo "MBA firmware link: $@"
+#	@mkdir -p $(dir $@)
+#	@rm -rf $@
+#	$(hide) ln -sf /firmware/radio/$(notdir $@) $@
+#
+#ALL_DEFAULT_INSTALLED_MODULES += $(MBA_SYMLINKS)
 
 # ADSP image symlink
 ADSP_IMAGES := \
--- releasetools/makelinks.sh
+++ releasetools/makelinks.sh
@@ -31,3 +31,20 @@ basedir="/system/blobs/$variant/"
 cd $basedir
 chmod 755 bin/*
 find . -type f | while read file; do ln -s $basedir$file /system/$file ; done
+
+# Create modem firmware links based on the currently installed modem
+mkdir -p /firmware/radio
+busybox mount -o shortname=lower -t vfat /dev/block/platform/msm_sdcc.1/by-name/radio /firmware/radio
+
+if ls /firmware/radio/a7b*.mdt 1> /dev/null 2>&1; then
+  base=`ls /firmware/radio/a91*.mdt | sort -r | head -1 | sed "s|.mdt||g"`
+elif [ -f "/firmware/radio/mba.mdt" ]; then
+  base="/firmware/radio/mba"
+fi
+
+ln -s $base.mdt /system/vendor/firmware/mba.mdt
+ln -s $base.b00 /system/vendor/firmware/mba.b00
+
+if [ ! -f "/system/vendor/firmware/mba.mdt" ]; then
+  exit 1
+fi
