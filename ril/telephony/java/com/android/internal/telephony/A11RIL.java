d0802ac53c1e4475542750db6203a080f89309e1
r3pwn (Greg Willard) <wickett06@gmail.com>
2015-09-26 00:07:13 -0400

    a11: Fix build

--- ril/telephony/java/com/android/internal/telephony/A11RIL.java
+++ ril/telephony/java/com/android/internal/telephony/A11RIL.java
@@ -6,36 +6,37 @@ import android.content.Context;
 import android.os.AsyncResult;
 import android.os.Message;
 import android.os.Parcel;
+import android.telephony.CellInfo;
 import android.telephony.Rlog;
 
-import com.android.internal.telephony.dataconnection.DataProfileOmh;
+import com.android.internal.telephony.uicc.IccIoResult;
+import com.android.internal.telephony.dataconnection.ApnProfileOmh;
+import com.android.internal.telephony.dataconnection.ApnSetting;
 import com.android.internal.telephony.dataconnection.DataProfile;
 
 import java.util.ArrayList;
 
-public class A11RIL extends RIL implements CommandsInterface {
+public class A11RIL extends RIL implements CommandsInterface
+{
 
-    static final int RIL_REQUEST_HTC_GET_DATA_CALL_PROFILE = 5505;
-    static final int RIL_REQUEST_HTC_SET_UICC_SUBSCRIPTION = 5506;
-    static final int RIL_REQUEST_HTC_SET_DATA_SUBSCRIPTION = 5507;
-    static final int RIL_UNSOL_HTC_UICC_SUBSCRIPTION_STATUS_CHANGED = 5760;
+    private static final int RIL_REQUEST_HTC_GET_DATA_CALL_PROFILE = 5505;
+    private static final int RIL_REQUEST_HTC_SET_UICC_SUBSCRIPTION = 5506;
+    private static final int RIL_REQUEST_HTC_ALLOW_DATA = 5507;
+    private static final int RIL_UNSOL_HTC_UICC_SUBSCRIPTION_STATUS_CHANGED = 5760;
 
-    public A11RIL(Context paramContext, int paramInt1,
-           int paramInt2, Integer paramInteger) {
+    public A11RIL(Context paramContext, int paramInt1, int paramInt2, Integer paramInteger)
+    {
          super(paramContext, paramInt1, paramInt2, paramInteger);
          mQANElements = 5;
     }
 
     @Override
-    public void
-    getDataCallProfile(int appType, Message result) {
-        RILRequest rr = RILRequest.obtain(
-                RIL_REQUEST_HTC_GET_DATA_CALL_PROFILE, result);
-
+    public void getDataCallProfile(int appType, Message result)
+    {
+        RILRequest rr = RILRequest.obtain(RIL_REQUEST_HTC_GET_DATA_CALL_PROFILE, result);
         // count of ints
         rr.mParcel.writeInt(1);
         rr.mParcel.writeInt(appType);
-
         if (RILJ_LOGD) {
             riljLog(rr.serialString() + "> " + requestToString(rr.mRequest)
                    + " : " + appType);
@@ -44,41 +45,39 @@ public class A11RIL extends RIL implements CommandsInterface {
     }
 
     @Override
-    public void setUiccSubscription(int slotId, int appIndex, int subId,
-            int subStatus, Message result) {
+    public void setUiccSubscription(int slotId, int appIndex, int subId, int subStatus, Message result)
+    {
         //Note: This RIL request is also valid for SIM and RUIM (ICC card)
         RILRequest rr = RILRequest.obtain(RIL_REQUEST_HTC_SET_UICC_SUBSCRIPTION, result);
-
         if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest)
                 + " slot: " + slotId + " appIndex: " + appIndex
                 + " subId: " + subId + " subStatus: " + subStatus);
-
         rr.mParcel.writeInt(slotId);
         rr.mParcel.writeInt(appIndex);
         rr.mParcel.writeInt(subId);
         rr.mParcel.writeInt(subStatus);
-
         send(rr);
     }
 
     @Override
-    public void setDataSubscription(Message result) {
-        RILRequest rr = RILRequest.obtain(RIL_REQUEST_HTC_SET_DATA_SUBSCRIPTION, result);
-        if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest));
+    public void setDataAllowed(boolean allowed, Message result)
+    {
+        RILRequest rr = RILRequest.obtain(RIL_REQUEST_HTC_ALLOW_DATA, result);
+        if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest)
+                + " " + allowed);
+        rr.mParcel.writeInt(1);
+        rr.mParcel.writeInt(allowed ? 1 : 0);
         send(rr);
     }
 
     @Override
-    protected void
-    processUnsolicited (Parcel p) {
+    protected void processUnsolicited (Parcel p)
+    {
         int dataPosition = p.dataPosition(); // save off position within the Parcel
         int response = p.readInt();
-
         if (response == RIL_UNSOL_HTC_UICC_SUBSCRIPTION_STATUS_CHANGED) {
             Object ret = responseInts(p);
-
             if (RILJ_LOGD) unsljLogRet(response, ret);
-
             if (mSubscriptionStatusRegistrants != null) {
                 mSubscriptionStatusRegistrants.notifyRegistrants(
                                     new AsyncResult (null, ret, null));
@@ -86,7 +85,6 @@ public class A11RIL extends RIL implements CommandsInterface {
         } else {
             // Rewind the Parcel
             p.setDataPosition(dataPosition);
-
             // Forward responses that we are not overriding to the super class
             super.processUnsolicited(p);
             return;
@@ -94,33 +92,27 @@ public class A11RIL extends RIL implements CommandsInterface {
     }
 
     @Override
-    protected RILRequest
-    processSolicited (Parcel p) {
+    protected RILRequest processSolicited (Parcel p)
+    {
         int serial, error;
         boolean found = false;
-
         serial = p.readInt();
         error = p.readInt();
-
         RILRequest rr;
-
         rr = findAndRemoveRequestFromList(serial);
-
         if (rr == null) {
             Rlog.w(RILJ_LOG_TAG, "Unexpected solicited response! sn: "
                             + serial + " error: " + error);
             return null;
         }
-
         Object ret = null;
-
         if (error == 0 || p.dataAvail() > 0) {
             // either command succeeds or command fails but with data payload
             try {switch (rr.mRequest) {
             /*
- cat libs/telephony/ril_commands.h \
- | egrep "^ *{RIL_" \
- | sed -re 's/\{([^,]+),[^,]+,([^}]+).+/case \1: ret = \2(p); break;/'
+             cat libs/telephony/ril_commands.h \
+             | egrep "^ *{RIL_" \
+             | sed -re 's/\{([^,]+),[^,]+,([^}]+).+/case \1: ret = \2(p); break;/'
              */
             case RIL_REQUEST_GET_SIM_STATUS: ret =  responseIccCardStatus(p); break;
             case RIL_REQUEST_ENTER_SIM_PIN: ret =  responseInts(p); break;
@@ -243,20 +235,31 @@ public class A11RIL extends RIL implements CommandsInterface {
             case RIL_REQUEST_GET_CELL_INFO_LIST: ret = responseCellInfoList(p); break;
             case RIL_REQUEST_SET_UNSOL_CELL_INFO_LIST_RATE: ret = responseVoid(p); break;
             case RIL_REQUEST_SET_INITIAL_ATTACH_APN: ret = responseVoid(p); break;
+            case RIL_REQUEST_SET_DATA_PROFILE: ret = responseVoid(p); break;
             case RIL_REQUEST_IMS_REGISTRATION_STATE: ret = responseInts(p); break;
             case RIL_REQUEST_IMS_SEND_SMS: ret =  responseSMS(p); break;
+            case RIL_REQUEST_SIM_TRANSMIT_APDU_BASIC: ret =  responseICC_IO(p); break;
+            case RIL_REQUEST_SIM_OPEN_CHANNEL: ret  = responseInts(p); break;
+            case RIL_REQUEST_SIM_CLOSE_CHANNEL: ret  = responseVoid(p); break;
+            case RIL_REQUEST_SIM_TRANSMIT_APDU_CHANNEL: ret = responseICC_IO(p); break;
+            case RIL_REQUEST_SIM_GET_ATR: ret = responseString(p); break;
+            case RIL_REQUEST_NV_READ_ITEM: ret = responseString(p); break;
+            case RIL_REQUEST_NV_WRITE_ITEM: ret = responseVoid(p); break;
+            case RIL_REQUEST_NV_WRITE_CDMA_PRL: ret = responseVoid(p); break;
+            case RIL_REQUEST_NV_RESET_CONFIG: ret = responseVoid(p); break;
             case RIL_REQUEST_HTC_SET_UICC_SUBSCRIPTION: ret = responseVoid(p); break;
-            case RIL_REQUEST_HTC_SET_DATA_SUBSCRIPTION: ret = responseVoid(p); break;
+            case RIL_REQUEST_HTC_ALLOW_DATA: ret = responseVoid(p); break;
+            case RIL_REQUEST_GET_HARDWARE_CONFIG: ret = responseHardwareConfig(p); break;
+            case RIL_REQUEST_SIM_AUTHENTICATION: ret =  responseICC_IOBase64(p); break;
+            case RIL_REQUEST_SHUTDOWN: ret = responseVoid(p); break;
             default:
                 throw new RuntimeException("Unrecognized solicited response: " + rr.mRequest);
             //break;
             }} catch (Throwable tr) {
                 // Exceptions here usually mean invalid RIL responses
-
                 Rlog.w(RILJ_LOG_TAG, rr.serialString() + "< "
                         + requestToString(rr.mRequest)
                         + " exception, possible invalid RIL response", tr);
-
                 if (rr.mResult != null) {
                     AsyncResult.forMessage(rr.mResult, null, tr);
                     rr.mResult.sendToTarget();
@@ -264,7 +267,13 @@ public class A11RIL extends RIL implements CommandsInterface {
                 return rr;
             }
         }
-
+        if (rr.mRequest == RIL_REQUEST_SHUTDOWN) {
+            // Set RADIO_STATE to RADIO_UNAVAILABLE to continue shutdown process
+            // regardless of error code to continue shutdown procedure.
+            riljLog("Response to RIL_REQUEST_SHUTDOWN received. Error is " +
+                    error + " Setting Radio State to Unavailable regardless of error.");
+            setRadioState(RadioState.RADIO_UNAVAILABLE);
+        }
         // Here and below fake RIL_UNSOL_RESPONSE_SIM_STATUS_CHANGED, see b/7255789.
         // This is needed otherwise we don't automatically transition to the main lock
         // screen when the pin or puk is entered incorrectly.
@@ -280,7 +289,6 @@ public class A11RIL extends RIL implements CommandsInterface {
                 }
                 break;
         }
-
         if (error != 0) {
             switch (rr.mRequest) {
                 case RIL_REQUEST_ENTER_SIM_PIN:
@@ -297,13 +305,10 @@ public class A11RIL extends RIL implements CommandsInterface {
                     }
                     break;
             }
-
             rr.onError(error, ret);
         } else {
-
             if (RILJ_LOGD) riljLog(rr.serialString() + "< " + requestToString(rr.mRequest)
                     + " " + retToString(rr.mRequest, ret));
-
             if (rr.mResult != null) {
                 AsyncResult.forMessage(rr.mResult, ret, null);
                 rr.mResult.sendToTarget();
@@ -312,13 +317,13 @@ public class A11RIL extends RIL implements CommandsInterface {
         return rr;
     }
 
-    static String
-    requestToString(int request) {
-/*
- cat libs/telephony/ril_commands.h \
- | egrep "^ *{RIL_" \
- | sed -re 's/\{RIL_([^,]+),[^,]+,([^}]+).+/case RIL_\1: return "\1";/'
-*/
+    static String requestToString(int request)
+    {
+    /*
+     cat libs/telephony/ril_commands.h \
+     | egrep "^ *{RIL_" \
+     | sed -re 's/\{RIL_([^,]+),[^,]+,([^}]+).+/case RIL_\1: return "\1";/'
+    */
         switch(request) {
             case RIL_REQUEST_GET_SIM_STATUS: return "GET_SIM_STATUS";
             case RIL_REQUEST_ENTER_SIM_PIN: return "ENTER_SIM_PIN";
@@ -432,22 +437,33 @@ public class A11RIL extends RIL implements CommandsInterface {
             case RIL_REQUEST_GET_CELL_INFO_LIST: return "RIL_REQUEST_GET_CELL_INFO_LIST";
             case RIL_REQUEST_SET_UNSOL_CELL_INFO_LIST_RATE: return "RIL_REQUEST_SET_CELL_INFO_LIST_RATE";
             case RIL_REQUEST_SET_INITIAL_ATTACH_APN: return "RIL_REQUEST_SET_INITIAL_ATTACH_APN";
+            case RIL_REQUEST_SET_DATA_PROFILE: return "RIL_REQUEST_SET_DATA_PROFILE";
             case RIL_REQUEST_IMS_REGISTRATION_STATE: return "RIL_REQUEST_IMS_REGISTRATION_STATE";
             case RIL_REQUEST_IMS_SEND_SMS: return "RIL_REQUEST_IMS_SEND_SMS";
+            case RIL_REQUEST_SIM_TRANSMIT_APDU_BASIC: return "RIL_REQUEST_SIM_TRANSMIT_APDU_BASIC";
+            case RIL_REQUEST_SIM_OPEN_CHANNEL: return "RIL_REQUEST_SIM_OPEN_CHANNEL";
+            case RIL_REQUEST_SIM_CLOSE_CHANNEL: return "RIL_REQUEST_SIM_CLOSE_CHANNEL";
+            case RIL_REQUEST_SIM_TRANSMIT_APDU_CHANNEL: return "RIL_REQUEST_SIM_TRANSMIT_APDU_CHANNEL";
+            case RIL_REQUEST_NV_READ_ITEM: return "RIL_REQUEST_NV_READ_ITEM";
+            case RIL_REQUEST_NV_WRITE_ITEM: return "RIL_REQUEST_NV_WRITE_ITEM";
+            case RIL_REQUEST_NV_WRITE_CDMA_PRL: return "RIL_REQUEST_NV_WRITE_CDMA_PRL";
+            case RIL_REQUEST_NV_RESET_CONFIG: return "RIL_REQUEST_NV_RESET_CONFIG";
             case RIL_REQUEST_HTC_SET_UICC_SUBSCRIPTION: return "RIL_REQUEST_SET_UICC_SUBSCRIPTION";
-            case RIL_REQUEST_HTC_SET_DATA_SUBSCRIPTION: return "RIL_REQUEST_SET_DATA_SUBSCRIPTION";
+            case RIL_REQUEST_HTC_ALLOW_DATA: return "RIL_REQUEST_ALLOW_DATA";
+            case RIL_REQUEST_GET_HARDWARE_CONFIG: return "GET_HARDWARE_CONFIG";
+            case RIL_REQUEST_SIM_AUTHENTICATION: return "RIL_REQUEST_SIM_AUTHENTICATION";
+            case RIL_REQUEST_SHUTDOWN: return "RIL_REQUEST_SHUTDOWN";
             default: return "<unknown request>";
         }
     }
 
-    static String
-    responseToString(int request)
+    static String responseToString(int request)
     {
-/*
- cat libs/telephony/ril_unsol_commands.h \
- | egrep "^ *{RIL_" \
- | sed -re 's/\{RIL_([^,]+),[^,]+,([^}]+).+/case RIL_\1: return "\1";/'
-*/
+    /*
+     cat libs/telephony/ril_unsol_commands.h \
+     | egrep "^ *{RIL_" \
+     | sed -re 's/\{RIL_([^,]+),[^,]+,([^}]+).+/case RIL_\1: return "\1";/'
+    */
         switch(request) {
             case RIL_UNSOL_RESPONSE_RADIO_STATE_CHANGED: return "UNSOL_RESPONSE_RADIO_STATE_CHANGED";
             case RIL_UNSOL_RESPONSE_CALL_STATE_CHANGED: return "UNSOL_RESPONSE_CALL_STATE_CHANGED";
@@ -488,36 +504,82 @@ public class A11RIL extends RIL implements CommandsInterface {
             case RIL_UNSOL_CELL_INFO_LIST: return "UNSOL_CELL_INFO_LIST";
             case RIL_UNSOL_RESPONSE_IMS_NETWORK_STATE_CHANGED:
                 return "UNSOL_RESPONSE_IMS_NETWORK_STATE_CHANGED";
-            case RIL_UNSOL_ON_SS: return "UNSOL_ON_SS";
-            case RIL_UNSOL_STK_CC_ALPHA_NOTIFY: return "UNSOL_STK_CC_ALPHA_NOTIFY";
-            case RIL_UNSOL_UICC_SUBSCRIPTION_STATUS_CHANGED:
             case RIL_UNSOL_HTC_UICC_SUBSCRIPTION_STATUS_CHANGED:
                     return "RIL_UNSOL_UICC_SUBSCRIPTION_STATUS_CHANGED";
-            case RIL_UNSOL_STK_SEND_SMS_RESULT: return "RIL_UNSOL_STK_SEND_SMS_RESULT";
-
+            case RIL_UNSOL_SRVCC_STATE_NOTIFY:
+                    return "UNSOL_SRVCC_STATE_NOTIFY";
+            case RIL_UNSOL_HARDWARE_CONFIG_CHANGED: return "RIL_UNSOL_HARDWARE_CONFIG_CHANGED";
+            case RIL_UNSOL_ON_SS: return "UNSOL_ON_SS";
+            case RIL_UNSOL_STK_CC_ALPHA_NOTIFY: return "UNSOL_STK_CC_ALPHA_NOTIFY";
             default: return "<unknown response>";
         }
     }
 
-    private ArrayList<DataProfile> responseGetDataCallProfile(Parcel p) {
+   protected Object responseHardwareConfig(Parcel p)
+   {
+      int num;
+      ArrayList<HardwareConfig> response;
+      HardwareConfig hw;
+      num = p.readInt();
+      response = new ArrayList<HardwareConfig>(num);
+      if (RILJ_LOGV) {
+         riljLog("responseHardwareConfig: num=" + num);
+      }
+      for (int i = 0 ; i < num ; i++) {
+         int type = p.readInt();
+         switch(type) {
+            case HardwareConfig.DEV_HARDWARE_TYPE_MODEM: {
+               hw = new HardwareConfig(type);
+               hw.assignModem(p.readString(), p.readInt(), p.readInt(),
+                  p.readInt(), p.readInt(), p.readInt(), p.readInt());
+               break;
+            }
+            case HardwareConfig.DEV_HARDWARE_TYPE_SIM: {
+               hw = new HardwareConfig(type);
+               hw.assignSim(p.readString(), p.readInt(), p.readString());
+               break;
+            }
+            default: {
+               throw new RuntimeException(
+                  "RIL_REQUEST_GET_HARDWARE_CONFIG invalid hardward type:" + type);
+            }
+         }
+         response.add(hw);
+      }
+      return response;
+   }
+
+   protected ArrayList<ApnSetting> responseGetDataCallProfile(Parcel p)
+   {
         int nProfiles = p.readInt();
         if (RILJ_LOGD) riljLog("# data call profiles:" + nProfiles);
-
-        ArrayList<DataProfile> response = new ArrayList<DataProfile>(nProfiles);
-
+        ArrayList<ApnSetting> response = new ArrayList<ApnSetting>(nProfiles);
         int profileId = 0;
         int priority = 0;
         for (int i = 0; i < nProfiles; i++) {
             profileId = p.readInt();
             priority = p.readInt();
-            DataProfileOmh profile = new DataProfileOmh(profileId, priority);
+            ApnProfileOmh profile = new ApnProfileOmh(profileId, priority);
             if (RILJ_LOGD) {
                 riljLog("responseGetDataCallProfile()" +
                         profile.getProfileId() + ":" + profile.getPriority());
             }
             response.add(profile);
         }
-
         return response;
+    }
+
+    protected Object responseICC_IOBase64(Parcel p)
+    {
+        int sw1, sw2;
+        Message ret;
+        sw1 = p.readInt();
+        sw2 = p.readInt();
+        String s = p.readString();
+        if (RILJ_LOGV) riljLog("< iccIO: "
+                + " 0x" + Integer.toHexString(sw1)
+                + " 0x" + Integer.toHexString(sw2) + " "
+                + s);
+        return new IccIoResult(sw1, sw2, android.util.Base64.decode(s, android.util.Base64.DEFAULT));
     }
 }
